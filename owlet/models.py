556d8465295e14d934857d723f32d3bbb1e58c0a
Datonex <cgomwe@ymail.com>
2022-05-07 16:32:53 +0100

    Remove side bar widget

--- owlet/models.py
+++ owlet/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import User
+# from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# class UserProfile(models.Model):
+#     profile = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="owlnook_user"
+#     )
+#     avatar_image = CloudinaryField("image", default="avatar")
+#     biography = models.TextField(verbose_name="biography", max_length=200, blank=True)
