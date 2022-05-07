e32eedbeb448665cdf4a24ca32b01614ed04a566
Datonex <cgomwe@ymail.com>
2022-05-07 16:09:02 +0100

    Add bookmark template + urls to template page

--- owlet/urls.py
+++ owlet/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
 
-from .views import UserDraftPostList
+from .views import UserDraftPostList, UserPostBookmarkList, PostBookmark
 
 urlpatterns = [
     path("<int:pk>/drafts/", UserDraftPostList.as_view(), name="draft_view"),
+    path("<int:pk>/bookmarks/", UserPostBookmarkList.as_view(), name="bookmark_view"),
+    path("bookmark/<slug:slug>/", PostBookmark.as_view(), name="post_bookmark"),
 ]
